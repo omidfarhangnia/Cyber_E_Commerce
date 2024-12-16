9648c1b89616bd267f066aa069042f28426c9af2
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2024-12-16 17:15:40 +0330

    start phase two

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,29 @@ import {
   HomeSec2,
   HomeSec3,
 } from "@/components/sections/home-sections";
+import { sql } from "@vercel/postgres";
 
-export default function Home() {
+async function getData() {
+  try {
+    const data = await sql`
+          SELECT * FROM invoicestwo
+      `;
+
+    return data.rows;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
     <div>
       {/* <HomeSec1 /> */}
       {/* <HomeSec2 /> */}
       {/* <HomeSec3 /> */}
+      <div className="bg-blue-500">hello there</div>
+      <div className="bg-yellow-300">{JSON.stringify(data)}</div>
     </div>
   );
 }
