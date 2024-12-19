8eb14da97fca8f2522c734b2d330921d4de240eb
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2024-12-19 20:38:19 +0330

    adding section 4

--- src/app/page.js
+++ src/app/page.js
@@ -2,32 +2,17 @@ import {
   HomeSec1,
   HomeSec2,
   HomeSec3,
+  HomeSec4,
 } from "@/components/sections/home-sections";
 import { sql } from "@vercel/postgres";
 
-async function getData() {
-  try {
-    const data = await sql`
-          SELECT name, id FROM products
-      `;
-
-    return data.rows;
-  } catch (err) {
-    throw new Error(err);
-  }
-}
-
 export default async function Home() {
-  const data = await getData();
   return (
     <div>
       {/* <HomeSec1 /> */}
       {/* <HomeSec2 /> */}
       {/* <HomeSec3 /> */}
-      <div className="bg-blue-500">hello there</div>
-      <div className="bg-yellow-300">{data.map((member) => {
-        return <div>{member.name} with id : {member.id}</div>
-      })}</div>
+      <HomeSec4 />
     </div>
   );
 }
